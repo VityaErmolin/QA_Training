0f5b1565f29a0cd5831801dd4b1a5d93f9766dc2
ViktorErmolin <viktorermolin@coherentsolutions.com>
2021-03-05 15:56:24 +0300

    Added allure

---
+++ Task90/Task70/App/Application.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Task70.Model;
+using Task70.Pages;
+
+namespace Task70.App
+{
+    public class Application
+    {
+        private readonly IWebDriver _driver;
+
+        private HomePage homePage;
+        private readonly LoginPage loginPage;
+
+        public Application()
+        {
+            _driver = new ChromeDriver();
+            loginPage = new LoginPage(_driver);
+        }
+
+        public void LoginUser(User user)
+        {
+            loginPage.Open();
+            MakeScreenShot();
+
+            homePage = loginPage.EnterClick()
+                .LoginAs(user.Login, user.Password)
+                .SubmitClick();
+        }
+
+        public void LogoutUser()
+        {
+            homePage = loginPage.Logout();
+        }
+
+        public bool LoggedIn()
+        {
+            return homePage.IsThisPage() && WaitElementDisplayed("uname");
+        }
+
+        public bool LoggedOut()
+        {
+            return homePage.IsThisPage() && WaitElementDisplayed("enter");
+        }
+
+        public void Quit()
+        {
+            _driver.Quit();
+        }
+
+        private bool WaitElementDisplayed(string className)
+        {
+            var waiter = new WebDriverWait(_driver, TimeSpan.FromMilliseconds(5000));
+
+            var element = waiter.Until(condition =>
+            {
+                try
+                {
+                    return _driver.FindElement(By.ClassName(className)).Displayed;
+                }
+                catch (InvalidElementStateException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+
+            return element;
+        }
+
+        public void MakeScreenShot()
+        {
+            ((ITakesScreenshot) _driver).GetScreenshot().SaveAsFile("Screenshot" + DateTime.Now.ToFileTime() + ".png");
+        }
+    }
+}
---
+++ Task90/Task70/Pages/LoginPage.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace Task70.Pages
+{
+    internal class LoginPage : Page
+    {
+        [FindsBy(How = How.XPath, Using = "//a[contains(@class,'auth__reg1')]")]
+        private IWebElement ButtonExit;
+
+        [FindsBy(How = How.Name, Using = "login")]
+        private IWebElement LoginInput;
+
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement PasswordInput;
+
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@class,'auth__enter')]")]
+        private IWebElement Submit;
+
+        [FindsBy(How = How.ClassName, Using = "uname")]
+        private IWebElement UserMenu;
+
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+
+        internal LoginPage Open()
+        {
+            _driver.Url = "https://www.tut.by/";
+            return this;
+        }
+
+        public LoginPage LoginAs(string username, string password)
+        {
+            LoginInput.SendKeys(username);
+            PasswordInput.SendKeys(password);
+            return this;
+        }
+
+        public LoginPage EnterClick()
+        {
+            _driver.FindElement(By.ClassName("enter")).Click();
+            return this;
+        }
+
+        public HomePage SubmitClick()
+        {
+            Submit.Click();
+            return new HomePage(_driver);
+        }
+
+        public HomePage Logout()
+        {
+            UserMenu.Click();
+            ButtonExit.Click();
+            return new HomePage(_driver);
+        }
+    }
+}
