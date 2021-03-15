22f516c221f639bdcce660b8bb79f8dbd89bf66b
ViktorErmolin <viktorermolin@coherentsolutions.com>
2021-03-15 14:14:41 +0300

    added test for node

---
+++ Task110/Task110/Task110/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace Task110
+{
+    public class Tests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new RemoteWebDriver(new Uri("http://10.10.104.62:4444/wd/hub"), new ChromeOptions());
+            _driver.Url = "https://www.tut.by/";
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var t = _driver.FindElement(By.ClassName("enter"));
+            Assert.True(t.Displayed);
+        }
+
+        [TearDown]
+        public void Close()
+        {
+
+            _driver.Quit();
+        }
+    }
+}
